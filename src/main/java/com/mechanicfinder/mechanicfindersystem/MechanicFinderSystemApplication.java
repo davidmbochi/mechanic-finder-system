f33001153fc203a4f140cd10956bdd712984f7a4
davidmbochi <davidmbochi97@gmail.com>
2021-11-04 13:41:47 +0300

    initial commit

---
+++ src/main/java/com/mechanicfinder/mechanicfindersystem/MechanicFinderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mechanicfinder.mechanicfindersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechanicFinderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MechanicFinderSystemApplication.class, args);
+	}
+
+}
