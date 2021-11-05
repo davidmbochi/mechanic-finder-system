6b60978641fd7ea8a6da689697de5c973b78a5bc
davidmbochi <davidmbochi97@gmail.com>
2021-11-05 16:09:08 +0300

    added task controller

--- src/main/java/com/mechanicfinder/mechanicfindersystem/controller/TaskController.java
+++ src/main/java/com/mechanicfinder/mechanicfindersystem/controller/TaskController.java
@@ -1,2 +1,23 @@
-package com.mechanicfinder.mechanicfindersystem.controller;public class TaskController {
+package com.mechanicfinder.mechanicfindersystem.controller;
+
+import com.mechanicfinder.mechanicfindersystem.model.Task;
+import com.mechanicfinder.mechanicfindersystem.service.TaskService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/api/task")
+@RestController
+@RequiredArgsConstructor
+public class TaskController{
+
+    private final TaskService taskService;
+
+    @GetMapping("/all")
+    public List<Task> findAllTasks(){
+        return taskService.findAllTasks();
+    }
 }
