83740732c7443ce6cf3142616f06c4b9d8f18ddb
davidmbochi <davidmbochi97@gmail.com>
2021-11-05 16:08:19 +0300

    added mechanic controller

--- src/main/java/com/mechanicfinder/mechanicfindersystem/controller/MechanicController.java
+++ src/main/java/com/mechanicfinder/mechanicfindersystem/controller/MechanicController.java
@@ -1,2 +1,22 @@
-package com.mechanicfinder.mechanicfindersystem.controller;public class MechanicController {
+package com.mechanicfinder.mechanicfindersystem.controller;
+
+import com.mechanicfinder.mechanicfindersystem.model.Mechanic;
+import com.mechanicfinder.mechanicfindersystem.service.MechanicService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/api/mechanic")
+@RestController
+@AllArgsConstructor
+public class MechanicController {
+    private final MechanicService mechanicService;
+
+    @GetMapping("/all")
+    public List<Mechanic> findAllMechanic(){
+        return mechanicService.findAllMechanics();
+    }
 }
