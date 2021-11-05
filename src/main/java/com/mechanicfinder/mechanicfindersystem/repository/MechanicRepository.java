672f58bd1b3bc98bcfbc09c083c260529865e6a6
davidmbochi <davidmbochi97@gmail.com>
2021-11-05 16:11:31 +0300

    added mechanic repository

--- src/main/java/com/mechanicfinder/mechanicfindersystem/repository/MechanicRepository.java
+++ src/main/java/com/mechanicfinder/mechanicfindersystem/repository/MechanicRepository.java
@@ -1,2 +1,8 @@
-package com.mechanicfinder.mechanicfindersystem.repository;public interface MechanicRepository {
+package com.mechanicfinder.mechanicfindersystem.repository;
+
+import com.mechanicfinder.mechanicfindersystem.model.Mechanic;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MechanicRepository extends JpaRepository<Mechanic, Long> {
+    Mechanic findMechanicByFirstName(String firstName);
 }
