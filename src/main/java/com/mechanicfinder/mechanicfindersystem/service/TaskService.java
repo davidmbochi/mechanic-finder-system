8caf6cdb533f24076bb6694773f339f2a061eb62
davidmbochi <davidmbochi97@gmail.com>
2021-11-05 16:13:45 +0300

    added task service

--- src/main/java/com/mechanicfinder/mechanicfindersystem/service/TaskService.java
+++ src/main/java/com/mechanicfinder/mechanicfindersystem/service/TaskService.java
@@ -1,2 +1,10 @@
-package com.mechanicfinder.mechanicfindersystem.service;public interface TaskService {
+package com.mechanicfinder.mechanicfindersystem.service;
+
+import com.mechanicfinder.mechanicfindersystem.model.Task;
+
+import java.util.List;
+
+public interface TaskService {
+    Task findTaskByTaskName(String taskName);
+    List<Task> findAllTasks();
 }
