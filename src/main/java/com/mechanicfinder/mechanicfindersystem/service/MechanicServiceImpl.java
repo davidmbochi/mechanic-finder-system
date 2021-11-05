4cab8d4d574661e27905830c7ac360b5901d3d95
davidmbochi <davidmbochi97@gmail.com>
2021-11-05 16:13:22 +0300

    added mechanic service implementation

--- src/main/java/com/mechanicfinder/mechanicfindersystem/service/MechanicServiceImpl.java
+++ src/main/java/com/mechanicfinder/mechanicfindersystem/service/MechanicServiceImpl.java
@@ -1,2 +1,22 @@
-package com.mechanicfinder.mechanicfindersystem.service;public class MechanicServiceImpl {
+package com.mechanicfinder.mechanicfindersystem.service;
+
+import com.mechanicfinder.mechanicfindersystem.model.Mechanic;
+import com.mechanicfinder.mechanicfindersystem.repository.MechanicRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MechanicServiceImpl implements MechanicService{
+
+    private final MechanicRepository mechanicRepository;
+
+    @Override
+    @Transactional
+    public List<Mechanic> findAllMechanics() {
+        return mechanicRepository.findAll();
+    }
 }
