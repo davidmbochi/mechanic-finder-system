4bcff3f03d7532d89d1bccd70d31c62a90ec8f02
davidmbochi <davidmbochi97@gmail.com>
2021-11-05 16:14:18 +0300

    added task service implementation

--- src/main/java/com/mechanicfinder/mechanicfindersystem/service/TaskServiceImpl.java
+++ src/main/java/com/mechanicfinder/mechanicfindersystem/service/TaskServiceImpl.java
@@ -1,2 +1,25 @@
-package com.mechanicfinder.mechanicfindersystem.service;public class TaskServiceImpl {
+package com.mechanicfinder.mechanicfindersystem.service;
+
+import com.mechanicfinder.mechanicfindersystem.model.Task;
+import com.mechanicfinder.mechanicfindersystem.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+@Service
+@RequiredArgsConstructor
+public class TaskServiceImpl implements TaskService{
+    private final TaskRepository taskRepository;
+
+    @Override
+    public Task findTaskByTaskName(String taskName) {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public List<Task> findAllTasks() {
+        return taskRepository.findAll();
+    }
 }
